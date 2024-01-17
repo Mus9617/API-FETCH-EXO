7d3ad7973e96ab0d7911d49a06dfd74ac2263655
Mus9617 <speede078@gmail.com>
2024-01-17 19:33:24 +0100

    Comentarios ñañadidos

--- createElements.js
+++ createElements.js
@@ -17,3 +17,11 @@ export function createTodoItem(todo) {
   todoItem.innerHTML = `Title: ${todo.title},<br>Completed: ${todo.completed}`;
   return todoItem;
 }
+
+
+
+
+
+// Bien aqui lo que hacemos es crear una funcion que va a crear 2 divs donde dentro tendremos los ul y li. y dentro tenemos las "id","name","username","email","street", "phone".
+// esas divs que creamos llama a cada id que necesitamos despues le damos un return debajo exportamos la funcion "createTodoIttem" donde dentro declaremos el "todo" que previemento 
+// emos declarado en el index.js despues vamos a declarar con el InnerHTML donde le decimos que nos saquue el "title","completed", y nos añadira la infa de la segundo API.
--- fetchData.js
+++ fetchData.js
@@ -9,3 +9,6 @@ export async function fetchTodos() {
   const todos = await response.json();
   return todos;
 }
+
+
+// Este simpelementa tal y como dice el nombre este codigo se dedica a obtener  la "Data" ya que con los links de la API saco los "Users" y "todos" y puede crear mis funcciones./////
